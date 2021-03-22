a7c8205560f6cc4dedc779951d16e51f9708a12c
江丽康 <22652@etransfar.com>
2021-03-22 17:39:41 +0800

    废弃Dynamic

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
 export { default as ItemBox } from './ItemBox';
 export { default as CheckButton } from './CheckButton';
 export { default as Tab } from './Tab';
-export { default as DynamicItem } from './DynamicItem';
